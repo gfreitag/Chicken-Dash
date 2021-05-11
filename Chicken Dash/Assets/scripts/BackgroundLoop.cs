c30d32d77f1f7adb46883ed2dd13c8e7d13206e7
Greta <greta.freitag@gmail.com>
2021-05-11 11:55:15 -0400

    populated arrays

--- Chicken Dash/Assets/scripts/BackgroundLoop.cs
+++ Chicken Dash/Assets/scripts/BackgroundLoop.cs
@@ -8,6 +8,7 @@ public class BackgroundLoop : MonoBehaviour
     private Vector3 lastScreenPosition;
     private Vector2 screenBounds;
     private Camera mainCamera;
+    public float scrollSpeed;
     public GameObject[] mountainsB;
     public GameObject[] mountainsFB;
     public GameObject[] fogB;
@@ -16,6 +17,8 @@ public class BackgroundLoop : MonoBehaviour
     public GameObject[] firB;
     public GameObject[] firF;
     public GameObject[] clouds;
+    public GameObject[] ground;
+    private GameObject[][] levels;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +26,20 @@ public class BackgroundLoop : MonoBehaviour
       mainCamera = gameObject.GetComponent<Camera>();
       screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
       lastScreenPosition = transform.position;
+      levels = new GameObject[][] {mountainsB, mountainsFB, fogB, mountainsF, fogF, firB, firF, clouds, ground};
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() {}
+
+    void repositionObjs(GameObject[] arr)
+    {
+        foreach(GameObject obj in arr)
+        {
+
+        }
+    }
+    void LateUpdate()
     {
 
     }
