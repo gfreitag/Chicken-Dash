1cbc99fd81f36e61f097269c9eac6eb58e9a525d
Greta <greta.freitag@gmail.com>
2021-05-11 12:15:45 -0400

    background loop

--- Chicken Dash/Assets/scripts/BackgroundLoop.cs
+++ Chicken Dash/Assets/scripts/BackgroundLoop.cs
@@ -18,6 +18,7 @@ public class BackgroundLoop : MonoBehaviour
     public GameObject[] firF;
     public GameObject[] clouds;
     public GameObject[] ground;
+    public GameObject[] sky;
     private GameObject[][] levels;
 
     // Start is called before the first frame update
@@ -26,7 +27,7 @@ public class BackgroundLoop : MonoBehaviour
       mainCamera = gameObject.GetComponent<Camera>();
       screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
       lastScreenPosition = transform.position;
-      levels = new GameObject[][] {mountainsB, mountainsFB, fogB, mountainsF, fogF, firB, firF, clouds, ground};
+      levels = new GameObject[][] {mountainsB, mountainsFB, fogB, mountainsF, fogF, firB, firF, clouds, ground, sky};
     }
 
     // Update is called once per frame
@@ -34,13 +35,25 @@ public class BackgroundLoop : MonoBehaviour
 
     void repositionObjs(GameObject[] arr)
     {
-        foreach(GameObject obj in arr)
+        float halfObjectWidth = arr[0].GetComponent<SpriteRenderer>().bounds.extents.x;
+        if(transform.position.x + screenBounds.x > arr[arr.Length-1].transform.position.x + halfObjectWidth)
         {
-
+            arr[0].transform.position = new Vector3(arr[arr.Length-1].transform.position.x + halfObjectWidth * 2,
+            arr[arr.Length-1].transform.position.y, arr[arr.Length-1].transform.position.z);
+            GameObject temp = arr[0];
+            for(int i=0; i<arr.Length-1; i++)
+            {
+              arr[i]=arr[i+1];
+            }
+            arr[arr.Length-1] = temp;
         }
     }
+
     void LateUpdate()
     {
-
+        foreach(GameObject[] arr in levels)
+        {
+            repositionObjs(arr);
+        }
     }
 }
