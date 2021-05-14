f1490fd808d52936b2f795ce925e3453b4b2800e
isabellakal <isabella.kalarickal@gmail.com>
2021-05-14 12:21:14 -0400

    update PlayerRun
    trying to use distance from starting position to change speed

--- Chicken Dash/Assets/Scripts/PlayerRun.cs
+++ Chicken Dash/Assets/Scripts/PlayerRun.cs
@@ -7,24 +7,30 @@ public class PlayerRun : MonoBehaviour
     // Start is called before the first frame update
     private float speed = 7.0f;
     private float distanceMoved ;
-    private float counter = 0f;
+    private float startPos;
+    private float currPos;
     void Start()
     {
+        startPos = transform.position.x;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Time.deltaTime*speed, 0,0);
-        counter = counter+1;
-         Debug.Log("distance:" + counter);
+        currPos = transform.position.x - startPos;
+        Debug.Log("current: " + currPos);
 
-         if (counter == 2000.0)
-         {
-             counter = 0f;
-             speed = speed + 0.5f;
-         }
+        if (currPos == 2000f) 
+        {
+            speed = speed + 0.5f;
+
+        }
+        transform.Translate(Time.deltaTime*speed, 0,0);
+
+        startPos = transform.position.x;
+
+
     }
 
 
