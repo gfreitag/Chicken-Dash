a9598d9b6a5a4c537e97cbb51b57475c3df3f606
Greta <greta.freitag@gmail.com>
2021-06-22 11:35:24 -0700

    test push

--- Chicken Dash/Assets/Scripts/PlayerRun.cs
+++ Chicken Dash/Assets/Scripts/PlayerRun.cs
@@ -9,20 +9,20 @@ public class PlayerRun : MonoBehaviour
     public float speed = 7.0f;
     public float acc = .07f;
     public Text textScore;
-    private int score = 0;
+    private int score = -1;
     //public ObstacleCol stopScore;
     //private float distanceMoved ;
     //private float startPos;
     //private float currPos;
-    
-    
+
+
     void Start()
     {
         //startPos = transform.position.x;
-        //calls distance function immediately upon starting and then calls it every 1/speed seconds 
+        //calls distance function immediately upon starting and then calls it every 1/speed seconds
         //score will increase at faster rate as speed increases in game
         InvokeRepeating("distance", 0, 1/speed);
-    
+
 
     }
 
@@ -38,7 +38,7 @@ public class PlayerRun : MonoBehaviour
         currPos = transform.position.x - startPos;
         Debug.Log("current: " + currPos);
 
-        if (currPos == 1000f) 
+        if (currPos == 1000f)
         {
             Debug.Log("CHANGING SPEED");
             speed = speed + 0.5f;
@@ -50,19 +50,19 @@ public class PlayerRun : MonoBehaviour
 
     }
 
-    void distance() 
+    void distance()
     {
         //update score (score is distance travelled)
            score++;
            textScore.text = score.ToString();
-    
-        
+
+
     }
 
 
    /* private void OnTriggerEnter(Collider obstacle)
     {
-        if (obstacle.tag == "obstacle") 
+        if (obstacle.tag == "obstacle")
         {
             Debug.Log("ouch!");
 
