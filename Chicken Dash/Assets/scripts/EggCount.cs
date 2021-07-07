d6e72d33a63ab0a304123e9d89cf068dbceac347
Greta <greta.freitag@gmail.com>
2021-07-07 14:45:21 -0700

    egg count permanence

--- Chicken Dash/Assets/scripts/EggCount.cs
+++ Chicken Dash/Assets/scripts/EggCount.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class EggCount : MonoBehaviour
 {
@@ -11,7 +14,7 @@ public class EggCount : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        eggCount = 0;
+        loadEggCount();
     }
 
     public int getEggCount()
@@ -24,4 +27,51 @@ public class EggCount : MonoBehaviour
         eggCount++;
         eggCounter.text = "x"+eggCount;
     }
+
+    public void refreshEggCount()
+    {
+        eggCounter.text = "x"+eggCount;
+    }
+
+    private void resetEggCount()
+    {
+        eggCount = 0;
+        saveEggCount();
+    }
+
+    public void saveEggCount()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath +  "/eggCount.dat");
+        EggData data = new EggData();
+        data.eggCount = eggCount;
+        bf.Serialize(file, data);
+        file.Close();
+        Debug.Log("File saved");
+    }
+
+    private void loadEggCount()
+    {
+        if(File.Exists(Application.persistentDataPath +  "/eggCount.dat"))
+        {
+            Debug.Log("File exists");
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath +  "/eggCount.dat", FileMode.Open);
+            EggData data = (EggData)bf.Deserialize(file);
+            file.Close();
+            eggCount = data.eggCount;
+            refreshEggCount();
+        }
+        else
+        {
+            Debug.Log("File does not exist");
+            eggCount = 0;
+        }
+    }
+}
+
+[Serializable]
+class EggData
+{
+    public int eggCount;
 }
